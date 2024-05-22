753a080953e39588db8fda7369d07956dd3bd03e
camioro <camioroo18@gmail.com>
2024-05-21 21:01:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const clientId = '1fe145a390ff4b7a8491a5db45858b13'; // Client ID proporcionado
+let token;
 
 const redirectUri = 'https://camioro.github.io/PlaylistCompass/'; // URL proporcionado
 const scopes = 'playlist-read-private playlist-read-collaborative';
-const authorizeUrl = `https://accounts.spotify.com/authorize?response_type=token&client_id=${clientId}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(redirectUri)}`;
+
+function obtenerToken() {
+  const hashParams = window.location.hash.substr(1).split('&');
+  for (let i = 0; i < hashParams.length; i++) {
+    const [key, value] = hashParams[i].split('=');
+    if (key === 'access_token') {
+      token = value;
+      break;
+    }
+  }
+}
 
 function obtenerIdCliente() {
   const usuario = document.getElementById('usuario').value;
@@ -13,9 +24,8 @@ function obtenerIdCliente() {
   })
   .then(response => response.json())
   .then(data => {
-    const clientId = data.client_id;
-    obtenerToken(clientId);
-    mostrarFotoPerfil(usuario);
+    mostrarFotoPerfil(data.images[0].url);
+    obtenerPlaylists(usuario);
   })
   .catch(error => {
     console.error('Error al obtener ID de cliente:', error);
@@ -23,25 +33,27 @@ function obtenerIdCliente() {
   });
 }
 
-function obtenerToken(clientId) {
-  const authorizeUrl = `https://accounts.spotify.com/authorize?response_type=token&client_id=${clientId}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(redirectUri)}`;
-  window.location = authorizeUrl;
+function mostrarFotoPerfil(urlFotoPerfil) {
+  document.getElementById('fotoPerfil').src = urlFotoPerfil;
 }
 
-function mostrarFotoPerfil(usuario) {
-  fetch(`https://api.spotify.com/v1/users/${usuario}`, {
+function obtenerPlaylists(usuario) {
+  fetch(`https://api.spotify.com/v1/users/${usuario}/playlists`, {
     headers: {
       'Authorization': 'Bearer ' + token
     }
   })
   .then(response => response.json())
   .then(data => {
-    const fotoPerfil = data.images[0].url;
-    document.getElementById('fotoPerfil').src = fotoPerfil;
+    const playlists = data.items.map(item => ({
+      nombre: item.name,
+      portada: item.images.length > 0 ? item.images[0].url : 'https://via.placeholder.com/150', // Si no hay imagen de portada, se usa una imagen de marcador de posición
+    }));
+    mostrarPlaylists(playlists);
   })
   .catch(error => {
-    console.error('Error al obtener la foto de perfil:', error);
-    alert('Error al obtener la foto de perfil');
+    console.error('Error al obtener playlists:', error);
+    alert('Error al obtener playlists');
   });
 }
 
@@ -57,4 +69,5 @@ function mostrarPlaylists(playlists) {
   document.getElementById('playlists').innerHTML = playlistHtml;
 }
 
-// Resto del código...
+// Función para obtener el token al cargar la página
+obtenerToken();
